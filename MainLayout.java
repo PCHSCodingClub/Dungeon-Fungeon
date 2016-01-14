d68505978137bd91a18e1fec67be6743fc7862dc
Darokrithia <dieltabin@gmail.com>
2016-01-14 15:13:05 -0700

    Create MainLayout.java

---
+++ MainLayout.java
@@ -0,0 +1,27 @@
+package com.blogspot.darokrithia.dungeonfungeon;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onCreditsClick(View v) {
+        if (v.getId() == R.id.CreditsButton) {
+            Intent i = new Intent(MainActivity.this, Credits.class);
+            startActivity(i);
+        }
+    }
+    public void onStartClick(View v) {
+        if (v.getId() == R.id.StartButton) {
+            Intent i = new Intent(MainActivity.this, Start.class);
+            startActivity(i);
+        }
+    }
+}
