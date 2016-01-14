aa63e3038984a9a36596a69cf4a2ec0c2f8c8428
HKeene17 <hkeene00@gmail.com>
2016-01-14 15:14:28 -0700

    Create CharacterSheet.java

---
+++ CharacterSheet.java
@@ -0,0 +1,23 @@
+package com.blogspot.darokrithia.dungeonfungeon;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by hkeene17 on 1/12/16.
+ */
+public class CharacterSheet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_charactersheet);
+    }
+
+public void onCreditsClick(View v) {
+    if (v.getId() == R.id.ArmorInventory) {
+        }
+    }
+}
