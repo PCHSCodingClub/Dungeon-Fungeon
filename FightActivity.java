ecd67d3b44f6b93ab388a299ee07a10e98798360
Darokrithia <dieltabin@gmail.com>
2016-01-14 15:11:39 -0700

    Create FightActivity.java

---
+++ FightActivity.java
@@ -0,0 +1,15 @@
+package com.blogspot.darokrithia.dungeonfungeon;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dtabin17 on 1/9/16.
+ */
+public class FightActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fight);
+    }
+}
